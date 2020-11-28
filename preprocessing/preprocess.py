000406bdc7af919d8776e32b8ab7910f01f65c87
dongju <dongju@dongjuui-MacBook-Pro.local>
2020-11-28 17:15:45 +0900

    Update for v3

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -13,8 +13,9 @@ for vehicle_type, csv_list in incheon_drone.items():
     graph     = np.loadtxt(csv_list[0], delimiter=',', dtype=np.float32)
     solution  = np.loadtxt(csv_list[1], delimiter=',', dtype=np.float32)
 
-    np.fill_diagonal(graph, -1)
+    #np.fill_diagonal(graph, -1)
+    graph[graph <= 0] = -1
     solution  = np.array(solution != 0).astype(int)
 
-    graph     = np.savetxt(csv_list[0].split('.')[0] + '_v2.csv', graph.astype(np.int), fmt='%i', delimiter=',')
-    solution  = np.savetxt(csv_list[1].split('.')[0].replace('_required', '_v2_required') + '.csv', solution.astype(np.int), fmt='%i', delimiter=',')
+    graph     = np.savetxt(csv_list[0].split('.')[0] + '_v3.csv', graph.astype(np.int), fmt='%i', delimiter=',')
+    solution  = np.savetxt(csv_list[1].split('.')[0].replace('_required', '_v3_required') + '.csv', solution.astype(np.int), fmt='%i', delimiter=',')
